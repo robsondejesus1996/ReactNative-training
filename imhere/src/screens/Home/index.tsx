c3f9ecb307cf02d26e5956990af3897191885836
Robson de Jesus <robsondejesus1996@hotmail.com>
2025-01-14 17:26:15 -0300

    feat: component participant

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,13 +1,20 @@
+import { Participant } from '@/src/components/Participant';
 import { styles } from './styles';
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 
 
 export const CONFIG = '';
 
 export function Home() {
 
+  const participants = ['Rodrigo', 'Robson', 'Diego', 'Caio', 'Ana', 'Maria', 'João', 'Pedro', 'Lucas', 'Gabriel', 'Rafael', 'Ricardo', 'Fernando', 'Juliana', 'Carla', 'Amanda', 'Bruna', 'Bianca', 'Camila', 'Isabela', 'Larissa', 'Mariana', 'Tatiana', 'Vivian', 'Yasmin', 'Zuleica'];
+
   function handleParticipantAdd() {
     console.log('Hello world!')
+  }
+
+  function handleParticipatRemove(name: string) {
+    alert(`removendo participante ${name}`)
   }
 
 
@@ -30,6 +37,23 @@ export function Home() {
           </Text>
         </TouchableOpacity>
       </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+      {
+        participants.map((participant => {
+          return (
+            <Participant
+            key={participant}
+            name={participant}
+            onRemove={() => handleParticipatRemove('Robson')} 
+            />
+          )
+        }))
+      }
+      </ScrollView>
+
+
+
     </View>
   )
 }
